043acfe17546d83eceb03347ef526dd5f2d3c168
Marti <marti@miden.team>
2025-07-05 15:16:00 +0200

    feat: Introduce `get_acct_procedure_ptr` method in `miden::memory` (#1530)
    * feat: add get_acct_procedure_ptr to memory mod
    
    * feat: replace section ptr with proc ptr call
    
    * chore: update kernel digests
    
    * Update crates/miden-lib/asm/kernels/transaction/lib/epilogue.masm

--- crates/miden-lib/src/transaction/procedures/kernel_v0.rs
+++ crates/miden-lib/src/transaction/procedures/kernel_v0.rs
@@ -72,7 +72,7 @@ pub const KERNEL0_PROCEDURES: [Digest; 36] = [
     // tx_get_block_timestamp
     digest!("0x786863e6dbcd5026619afd3831b7dcbf824cda54950b0e0724ebf9d9370ec723"),
     // tx_start_foreign_context
-    digest!("0x50e31990bc918903f0babea20d4eb840828fdd41b539adb98f70cde6fffa8bfc"),
+    digest!("0x20f874f328809be9174240a4272f8f1a41437b22662c1790109e5ad7f201cd40"),
     // tx_end_foreign_context
     digest!("0x90a107168d81c1c0c23890e61fb7910a64b4711afd0bf8c3098d74737e4853ba"),
     // tx_get_expiration_delta
